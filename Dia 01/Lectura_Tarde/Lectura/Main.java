5ea5dc4657fc2ac751be7d05029a2b332d0f9d1d
Doomlash <emivilche123@gmail.com>
2022-04-04 15:11:33 -0300

    Reorganizada carpeta Lectura_Tarde

--- Dia 01/Lectura_Tarde/Lectura/Main.java
+++
@@ -1,8 +0,0 @@
-package Lectura_Tarde.Lectura;
-
-public class Main {
-    public static void main(String[] args) {
-        Triangulo t = new Triangulo(5,5,5);
-        System.out.println(t.tipoDeTriangulo());
-    }
-}
