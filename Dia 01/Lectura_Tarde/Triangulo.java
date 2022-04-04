3a054caf299405c9029264ab3aef79c1890b8374
Doomlash <emivilche123@gmail.com>
2022-04-04 15:11:12 -0300

    Reorganizadas la carpeta Lectura_Tarde

---
+++ Dia 01/Lectura_Tarde/Triangulo.java
@@ -0,0 +1,29 @@
+package Lectura_Tarde;
+
+public class Triangulo {
+    private float ladoA, ladoB, ladoC;
+
+    public Triangulo(float ladoA, float ladoB, float ladoC){
+        this.ladoA = ladoA;
+        this.ladoB = ladoB;
+        this.ladoC = ladoC;
+    }
+
+    public String tipoDeTriangulo(){
+        if (ladoA == ladoB) {
+            if (ladoB == ladoC){
+                return "Equilatero";
+            }
+            else{
+                return "Isosceles";
+            }
+        } else {
+            if (ladoB == ladoC){
+                return "Isosceles";
+            }
+            else{
+                return "Escaleno";
+            }
+        }
+    }
+}
